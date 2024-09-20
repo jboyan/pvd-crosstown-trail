2833c0e7850a38f195156611e323c3177a6c10a6
jboyan <t8QjEtDRZa>
2024-09-20 10:38:53 -0400

    fix warning

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,12 +1,10 @@
-import App from "./App";
-
 const NotFound = () => {
-  return (
-    <div>
-      <h2>404: Page Not Found</h2>
-      <p>The page you are looking for does not exist.</p>
-    </div>
-  );
+    return (
+        <div>
+            <h2>404: Page Not Found</h2>
+            <p>The page you are looking for does not exist.</p>
+        </div>
+    );
 };
 
 export default NotFound;
