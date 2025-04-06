28b9bc023a617d711a3a39190dc303192a70ea71
jboyan <t8QjEtDRZa>
2025-04-06 08:28:00 -0400

    update PPS hike date

--- src/crosstown/home.js
+++ src/crosstown/home.js
@@ -21,7 +21,7 @@ export default function Home() {
 
             <Button variant={"contained"} style={{marginBottom: '20px'}} target="_blank"
                     component="a" href="https://ppsri.org/events/walk-the-pvd-crosstown-trail-a-13-mile-urban-hike/">
-                Join our second Public Hike on Sunday, April 6!
+                Join our second Public Hike on &nbsp;<strike>Sunday, April 6!</strike> (to be rescheduled for May)
             </Button>
 
             <Typography variant="body1" style={{textAlign: 'left', marginBottom: '20px'}}>
